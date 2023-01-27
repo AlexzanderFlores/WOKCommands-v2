da4b6ce70cd93b4fe29a28e456476b6384dbfec9
Dominik Mandinec <mandinec53@gmail.com>
2023-01-27 18:19:11 +0100

    dist

--- dist/command-handler/validations/syntax/callback-required.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (command) => {
-    const { commandObject, commandName } = command;
-    if (!commandObject.callback) {
-        throw new Error(`Command "${commandName}" does not have a callback function.`);
-    }
-};
--- dist/command-handler/validations/syntax/test-without-server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (command) => {
-    const { instance, commandName, commandObject } = command;
-    if (commandObject.testOnly !== true || instance.testServers.length) {
-        return;
-    }
-    throw new Error(`Command "${commandName}" is a test only command, but no test servers were specified.`);
-};
