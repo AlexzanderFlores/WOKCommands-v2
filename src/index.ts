8ad39b9a782b5813615dbf277434950cc18e80c2
Dominik Mandinec <mandinec53@gmail.com>
2023-01-28 18:10:45 +0100

    export ConfigType

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import CommandType from "./util/CommandType";
 import CooldownTypes from "./util/CooldownTypes";
 import DefaultCommands from "./util/DefaultCommands";
 import DbModels from "./models/index-model";
+import ConfigType from "./util/ConfigType";
 
 module.exports = WOK;
 module.exports.CommandType = CommandType;
+module.exports.ConfigType = ConfigType;
 module.exports.CooldownTypes = CooldownTypes;
 module.exports.DefaultCommands = DefaultCommands;
 module.exports.DbModels = DbModels;
